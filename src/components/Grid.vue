70f452a5db6b646ec7bc79557716904434ea9558
DenisGNL <denis.gnl@gmail.com>
2023-01-09 20:59:10 -0300

    updated grid component to use composition API

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,64 +1,58 @@
 <script setup>
 import Tile from "../components/Tile.vue"
+import { onMounted, reactive } from "vue";
+
+const col = 7
+const row = 7
+const bombs = 10
+let grid = reactive([])
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+function  plantBomb() {
+    const colb = getRandomInt(col+1)
+    const rowb = getRandomInt(row+1)
+    if(grid[colb][rowb]){
+        plantBomb()
+    }else{
+        grid[colb][rowb] = true
+    }
+}
+function spreadBomb() {
+    for(let i = 1;i <= bombs; i++){
+        plantBomb()
+    }
+}
+function createGrid() {
+    for (let i = 0; i <= col; i++) {
+        grid[i] = []
+        for (let j = 0; j <= row; j++) {
+            grid[i][j] = false
+        }
+    }
+    spreadBomb()
+}
+function changeGrid(x, y) {
+    grid[x][y] = !grid[x][y]
+}
+
+onMounted(() => {
+   createGrid()
+})
+
+
 </script>
 
 <template> 
     <div :style="{ 'grid-template-columns': 'repeat( '+(col+1)+', minmax(0, 1fr))'}" class="grid w-fit">
-        <div v-for="(col, x_index) in grid" :key="x_index" class="grid w-fit">
-            <div v-for="(state, y_index) in col" :key="y_index" class="grid w-fit">
+        <div v-for="(column, x_index) in grid" :key="x_index" class="grid w-fit">
+            <div v-for="(state, y_index) in column" :key="y_index" class="grid w-fit">
                 <Tile @showTile="changeGrid" :state="state" :x="x_index" :y="y_index"/>
             </div>
         </div>
     </div>
 </template>
-
-<script>
-export default {
-    data() {
-        return {
-            col: 7,
-            row: 7,
-            bombs: 10,
-            grid: [],
-        }
-    },
-    methods: {
-        getRandomInt(max) {
-            return Math.floor(Math.random() * max);
-        },
-        createGrid() {
-            for (let i = 0; i <= this.col; i++) {
-                this.grid[i] = []
-                for (let j = 0; j <= this.row; j++) {
-                    this.grid[i][j] = false
-                }
-            }
-            this.spreadBomb()
-        },
-        plantBomb() {
-            const col = this.getRandomInt(this.col+1)
-            const row = this.getRandomInt(this.row+1)
-            if(this.grid[col][row]){
-                this.plantBomb()
-            }else{
-                this.grid[col][row] = true
-            }
-        },
-        spreadBomb() {
-            for(let i = 1;i <= this.bombs; i++){
-                this.plantBomb()
-            }
-        },
-        changeGrid(x, y) {
-            this.grid[x][y] = !this.grid[x][y]
-        },
-    },   
-    mounted() {
-        this.createGrid();
-
-    }
-}
-</script>
 
 <style scoped>
 img {
